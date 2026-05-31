b03a94ed676995a9bef197c7789d4337a3e99d46
Jeswin <jeswinpk@agilehead.com>
2026-05-31 23:17:33 +0530

    Add checker name resolution parity slice

--- packages/tsts/src/checker/index.ts
+++ packages/tsts/src/checker/index.ts
@@ -1,6 +1,7 @@
 export * from "./checker.js";
 export * from "./checkerInitialization.js";
 export * from "./checkerCore.js";
+export * as nameResolution from "./checker.nameResolution.js";
 export * from "./nodeBuilder.js";
 export * from "./nodeBuilderHover.js";
 export * from "./nodeBuilderImpl.js";
