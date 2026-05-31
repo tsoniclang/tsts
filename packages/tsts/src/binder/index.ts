bb4476c405e853cd574fc9c0f59510fe838efa90
Jeswin <jeswinpk@agilehead.com>
2026-05-31 11:54:57 +0530

    Complete binder parity support surfaces

--- packages/tsts/src/binder/index.ts
+++ packages/tsts/src/binder/index.ts
@@ -7,3 +7,9 @@ export {
 } from "./binder.js";
 export * from "./nameresolver.js";
 export * from "./referenceresolver.js";
+export * from "./flow.js";
+export * from "./strictmode.js";
+export * as commonjs from "./commonjs.js";
+export * as container from "./container.js";
+export * as labels from "./labels.js";
+export * as scope from "./scope.js";
