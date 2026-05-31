0b68dba793b6120ecf6e8f783a06e3a4740dd11d
Jeswin <jeswinpk@agilehead.com>
2026-05-31 23:34:32 +0530

    Add checker module export parity

--- packages/tsts/src/checker/index.ts
+++ packages/tsts/src/checker/index.ts
@@ -2,6 +2,7 @@ export * from "./checker.js";
 export * from "./checkerInitialization.js";
 export * from "./checkerCore.js";
 export * as nameResolution from "./checker.nameResolution.js";
+export * as moduleChecks from "./checker.moduleChecks.js";
 export * from "./nodeBuilder.js";
 export * from "./nodeBuilderHover.js";
 export * from "./nodeBuilderImpl.js";
