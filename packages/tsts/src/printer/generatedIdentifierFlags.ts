f4503ee6b09a54af0bf804dd3ded768700154afe
Jeswin <jeswinpk@agilehead.com>
2026-05-31 20:18:15 +0530

    Expand generated identifier flag helpers

--- packages/tsts/src/printer/generatedIdentifierFlags.ts
+++ packages/tsts/src/printer/generatedIdentifierFlags.ts
@@ -7,5 +7,43 @@
  * import surface.
  */
 
+import { GeneratedIdentifierFlags } from "./nameGenerator.js";
+
 export { GeneratedIdentifierFlags } from "./nameGenerator.js";
 export type { GeneratedIdentifierFlags as GeneratedIdentifierFlagsType } from "./nameGenerator.js";
+
+export function generatedIdentifierKind(flags: GeneratedIdentifierFlags): GeneratedIdentifierFlags {
+  return flags & GeneratedIdentifierFlags.KindMask;
+}
+
+export function generatedIdentifierIsAuto(flags: GeneratedIdentifierFlags): boolean {
+  return generatedIdentifierKind(flags) === GeneratedIdentifierFlags.Auto;
+}
+
+export function generatedIdentifierIsLoop(flags: GeneratedIdentifierFlags): boolean {
+  return generatedIdentifierKind(flags) === GeneratedIdentifierFlags.Loop;
+}
+
+export function generatedIdentifierIsUnique(flags: GeneratedIdentifierFlags): boolean {
+  return generatedIdentifierKind(flags) === GeneratedIdentifierFlags.Unique;
+}
+
+export function generatedIdentifierIsNode(flags: GeneratedIdentifierFlags): boolean {
+  return generatedIdentifierKind(flags) === GeneratedIdentifierFlags.Node;
+}
+
+export function generatedIdentifierIsReservedInNestedScopes(flags: GeneratedIdentifierFlags): boolean {
+  return (flags & GeneratedIdentifierFlags.ReservedInNestedScopes) !== 0;
+}
+
+export function generatedIdentifierIsOptimistic(flags: GeneratedIdentifierFlags): boolean {
+  return (flags & GeneratedIdentifierFlags.Optimistic) !== 0;
+}
+
+export function generatedIdentifierIsFileLevel(flags: GeneratedIdentifierFlags): boolean {
+  return (flags & GeneratedIdentifierFlags.FileLevel) !== 0;
+}
+
+export function generatedIdentifierHasAllowNameSubstitution(flags: GeneratedIdentifierFlags): boolean {
+  return (flags & GeneratedIdentifierFlags.AllowNameSubstitution) !== 0;
+}
