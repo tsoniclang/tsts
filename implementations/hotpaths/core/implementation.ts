b6e091fe18a06014c00455b807f33cc0ce8a2851
Jeswin <jeswinpk@agilehead.com>
2026-08-30 15:01:25 +0530

    perf(core): fuse arena element locations

--- implementations/hotpaths/core/implementation.ts
+++ implementations/hotpaths/core/implementation.ts
@@ -13,6 +13,36 @@ import {
 import { GoPanic } from "@gotots/runtime/panic.js";
 import { allocatePointer, loadPointer } from "@tsonic/core/lang.js";
 
+class ArenaElementLocation<F, T> implements Pointer<T> {
+  declare readonly __tsonicSourceType: (value: T) => T;
+
+  readonly storageIdentity: object;
+  readonly storageKey: PropertyKey;
+
+  constructor(
+    private readonly source: RuntimeSlice<F>,
+    private readonly index: int,
+    private readonly fromSource: (value: F) => T,
+    private readonly toSource: (value: T) => F,
+  ) {
+    const numericIndex = globalThis.Number(index);
+    const location = source.$arrayLocation(numericIndex + 1);
+    if (location === undefined) {
+      GoPanic.raiseRuntime("slice bounds out of range");
+    }
+    this.storageIdentity = location[0];
+    this.storageKey = location[1] + numericIndex;
+  }
+
+  get value(): T {
+    return this.fromSource(this.source.get(this.index));
+  }
+
+  set value(value: T) {
+    this.source.set(this.index, this.toSource(value));
+  }
+}
+
 export function arenaNew<T>(
   arenaPointer: Pointer<Arena<T>> | undefined,
   sliceCapacity: (slice: RuntimeSlice<GoContainerStorage<T>>) => int,
@@ -21,7 +51,7 @@ export function arenaNew<T>(
   ) => RuntimeSlice<GoContainerStorage<T>>,
   copyValue: (value: T) => T,
   fromContainerStorage: (value: GoContainerStorage<T>) => T,
-  indexAddress: (
+  _indexAddress: (
     slice: RuntimeSlice<GoContainerStorage<T>>,
     index: int,
   ) => Pointer<T> | undefined,
@@ -56,7 +86,12 @@ export function arenaNew<T>(
   const index = sliceLength(data);
   data = data.slice(0, index + 1, null);
   arena.data = data;
-  return indexAddress(data, index);
+  return new ArenaElementLocation(
+    data,
+    index,
+    fromContainerStorage,
+    toContainerStorage,
+  );
 }
 
 export function linkStoreGet<K, V>(
